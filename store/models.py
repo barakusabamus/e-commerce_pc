434f07a2413bce8e66cce2e782915c87e412758c
barakusabamus <villa222csgo1313@gmail.com>
2022-08-09 20:51:45 +0300

    create and register model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Computer(models.Model):
+    title = models.CharField(max_length=30, null=True)
+    description = models.TimeField(null=True)
+    gpu = models.CharField(max_length=30, null=True)
+    cpu = models.CharField(max_length=30, null=True)
+    ram = models.CharField(max_length=30, null=True)
+    mother_board = models.CharField(max_length=30, null=True)
+    ssd = models.CharField(max_length=30, null=True)
+    hdd = models.CharField(max_length=30, null=True)
+    psu = models.CharField(max_length=30, null=True)
+    case = models.CharField(max_length=30, null=True)
+    cooling = models.CharField(max_length=30, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
