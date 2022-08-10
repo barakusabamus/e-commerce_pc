ec97cbac46a038893e25a2a8c4ea3eefcb0f0259
barakusabamus <villa222csgo1313@gmail.com>
2022-08-10 12:10:49 +0300

    create and register model Photo

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
-admin.site.register(models.Computer)
+
+class PhotoInline(admin.TabularInline):
+    model = models.Photo
+
+
+class ComputeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'gpu', 'cpu', 'price', )
+    list_display_links = ('id', 'title', 'gpu', 'cpu', 'price',)
+    inlines = [PhotoInline]
+
+
+admin.site.register(models.Computer, ComputeAdmin)
+admin.site.register(models.Photo)
--- store/models.py
+++ store/models.py
@@ -15,3 +15,13 @@ class Computer(models.Model):
     case = models.CharField(max_length=30, null=True)
     cooling = models.CharField(max_length=30, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+
+class Photo(models.Model):
+    computer = models.ForeignKey('Computer', null=True, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_active = models.BooleanField(default=False, null=True)
+    is_main = models.BooleanField(default=False)
