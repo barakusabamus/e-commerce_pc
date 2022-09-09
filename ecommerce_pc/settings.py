14d233ceab173a0539742639d9ac539af0bbe2ec
barakusabamus <villa222csgo1313@gmail.com>
2022-09-09 15:39:13 +0300

    add media settings

--- ecommerce_pc/settings.py
+++ ecommerce_pc/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'blo'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,4 +119,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "photos")
 
+MEDIA_URL = '/photos/'
